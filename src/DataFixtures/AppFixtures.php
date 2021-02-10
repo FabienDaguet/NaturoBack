577ab91c4ab5aa0bddc90657184332a40aa54145
fabien daguet <daguet77@gmail.com>
2021-02-10 19:04:15 +0100

    generate fake user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker;
+use App\Entity\Users;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
+        
+    
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
 
+        $generator = Faker\Factory::create("fr_FR");
+        for ($i = 0; $i < 1; $i++){
+            $user = new Users();
+            $password = $this->encoder->encodePassword($user , 'password');
+            $user
+            ->setFirstName($generator->firstName())
+            ->setLastName($generator->lastName)
+            ->setEmail($generator->email)
+            ->setPassword($password);
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
