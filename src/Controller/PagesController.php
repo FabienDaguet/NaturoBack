4dab02d1e04b6cb7b1c23b2a0e49fa7a86bc3f66
fabien daguet <daguet77@gmail.com>
2021-02-12 17:03:58 +0100

    create all link and view for all 'Pages'

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -9,12 +9,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class PagesController extends AbstractController
 {
     /**
-     * @Route("/", name="pages")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('pages/index.html.twig', [
             'controller_name' => 'PagesController',
         ]);
+    }
+
+    /**
+     * @Route("/histoire", name="history")
+     */
+    public function history(): Response
+    {
+        return $this->render('pages/history.html.twig');
+    }
+
+    /**
+     * @Route("/Qui-suis-je", name="aboutMe")
+     */
+    public function aboutME(): Response
+    {
+        return $this->render('pages/aboutMe.html.twig');
+    }
+
+    /**
+     * @Route("/séance", name="seance")
+     */
+    public function seance(): Response
+    {
+        return $this->render('pages/seance.html.twig');
+    }
+
+    /**
+     * @Route("/tarifs", name="prices")
+     */
+    public function prices(): Response
+    {
+        return $this->render('pages/prices.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig');
     }
 }
